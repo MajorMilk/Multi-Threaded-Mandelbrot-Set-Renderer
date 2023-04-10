94ef6246ecf3a8ad19b96ff4039c5a9abb9b5d16
MajorMilk <115612092+MajorMilk@users.noreply.github.com>
2023-04-09 21:43:51 -0400

    Newtons Fractal support

---
+++ NewtonsFractal/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+int width = 4000, height = 4000;
+
+Bitmap bitmap = new Bitmap(width, height);
+
+ 
+int exponent = 3;
+
+int theadCount = 8;
+int threadWidth = width / theadCount;
+
+Thread[] threads = new Thread[theadCount];
+
+double Map(double num, double min1, double max1, double min2, double max2)
+{
+    return (num - min1) * (max2 - min2) / (max1 - min1) + min2;
+}
+
+Color GetColorForPixel(Complex pixel, int maxIterations, double threshold)
+{
+    // Set up the roots of the polynomial
+    Complex[] roots = { 1, Complex.FromPolarCoordinates(1, 2 * Math.PI / 3), Complex.FromPolarCoordinates(1, -2 * Math.PI / 3) };
+
+    // Initialize variables for tracking the root and iteration count
+    Complex root = Complex.Zero;
+    int iterations = 0;
+
+    // Iterate the function until it converges or the maximum number of iterations is reached
+    while (iterations < maxIterations && Complex.Abs(Complex.Pow(pixel,exponent)-1) > threshold)
+    {
+        // Calculate the next iteration using the Newton-Raphson method
+        root = roots.OrderBy(r => Complex.Abs(pixel - r)).First();
+        pixel -= (Complex.Pow(pixel,exponent)-1) / (exponent * Complex.Pow(pixel,exponent-1));
+        iterations++;
+    }
+
+    // Interpolate the color of the pixel based on the root it converges to
+    Color color;
+    if (root == 1)
+    {
+        color = Color.Red; // root 1
+    }
+    else if (root == Complex.FromPolarCoordinates(1, 2 * Math.PI / 3))
+    {
+        color = Color.Green; // root 2
+    }
+    else
+    {
+        color = Color.Blue; // root 3
+    }
+
+    return color;
+}
+
+
+
+
+for(int x = 0; x < width; x++)
+{
+    for(int y = 0; y < height; y++)
+    {
+        double a = Map(x, 0, width, -2, 2);
+        double b = Map(y, 0, height, -2, 2);
+
+        Complex z = new Complex(a, b);
+
+        bitmap.SetPixel(x, y, GetColorForPixel(z, 100, 0.0001));
+
+        
+    }
+}
+
+bitmap.Save("output.png");
--- README.md
+++ README.md
@@ -4,6 +4,14 @@
  at the moment things are set up to be in a 1:1 aspect ratio but by changing the ratio between the x and y bounds to match the screens ratio, you can make it any aspect ratio
 
  you can also slide the window around by adjusting the x and y offsets;
+ 
+ # Update 2: Newtons fractal
+ 
+ Its in a side project and isnt multithreaded but it does support changing the exponent of the polynomial with a single variable. 
+ 
+ if you wanted to change the polynomial further the iterations are done in the generate color function.
+ 
+![image](https://user-images.githubusercontent.com/115612092/230808566-3784d86b-9778-45d7-a11a-16d2efcb88cf.png)
 
 # Update
  
@@ -13,4 +21,5 @@
 
 
 ![image](https://user-images.githubusercontent.com/115612092/230722694-1c22c89b-fe69-4087-8a9e-18d5034f59a7.png)
+
 
